0d36bf848da40628de5488d278c98fc2de3fb569
m-hatch <montehatch@gmail.com>
2018-03-27 23:29:43 -0400

    scope vars

--- main.js
+++ main.js
@@ -1,90 +1,92 @@
+(function(){
+  // DOM elements
+  const wordInput = document.querySelector('.word__input');
+  const wordtype = document.querySelector('.word__wordtype');
+  const button = document.querySelector('.continue-btn');
+  const entry = document.querySelector('.entry');
+  const sillyStory = document.querySelector('.story');
 
-// DOM elements
-const wordInput = document.querySelector('.word__input');
-const wordtype = document.querySelector('.word__wordtype');
-const button = document.querySelector('.continue-btn');
-const entry = document.querySelector('.entry');
-const sillyStory = document.querySelector('.story');
+  // vars
+  const stories = [];
+  let telling;
+  let answers = [];
 
-// vars
-const stories = [];
-let telling;
-let answers = [];
-
-class Story {
-  constructor(title, text, words) {
-    this.id = Story.autoincrement();
-    this.title = title;
-    this.text = text;
-    this.words = words;
-  }
-
-  //static class to generate ids
-  static autoincrement() {
-    Story.id = Story.id || 0;
-    Story.id++;
-    return Story.id;
-  }
-
-  showStory() {
-    let html = '';
-    entry.setAttribute('style', 'display: none');
-    this.text.forEach((line, index, arr) => {
-      html += line;
-      if (index < arr.length - 1)
-        html += answers[index];
-    });
-    sillyStory.innerHTML = html;
-  }
-}
-
-class Words {
-  constructor(types) {
-    this.index = 0;
-    this.types = types;
-    this.askForWord = this.askForWord.bind(this);
-    this.saveAnswer = this.saveAnswer.bind(this);
-  }
-
-  askForWord() {
-    if (this.index === this.types.length - 1) {
-      button.innerHTML = 'Get Silly Story';
+  class Story {
+    constructor(title, text, words) {
+      this.id = Story.autoincrement();
+      this.title = title;
+      this.text = text;
+      this.words = words;
     }
-    if (this.index < this.types.length) {
-      wordtype.innerHTML = this.types[this.index];
-      wordInput.focus();
-      this.index++;
-    } else {
-      telling.showStory();
+
+    //static class to generate ids
+    static autoincrement() {
+      Story.id = Story.id || 0;
+      Story.id++;
+      return Story.id;
+    }
+
+    showStory() {
+      let html = '';
+      entry.setAttribute('style', 'display: none');
+      this.text.forEach((line, index, arr) => {
+        html += line;
+        if (index < arr.length - 1)
+          html += answers[index];
+      });
+      sillyStory.innerHTML = html;
     }
   }
 
-  saveAnswer() {
-    if (this.index > 0) {
-      answers.push(wordInput.value || '');
+  class Words {
+    constructor(types) {
+      this.index = 0;
+      this.types = types;
+      this.askForWord = this.askForWord.bind(this);
+      this.saveAnswer = this.saveAnswer.bind(this);
     }
-    wordInput.value = '';
-    this.askForWord();
+
+    askForWord() {
+      if (this.index === this.types.length - 1) {
+        button.innerHTML = 'Get Silly Story';
+      }
+      if (this.index < this.types.length) {
+        wordtype.innerHTML = this.types[this.index];
+        wordInput.focus();
+        this.index++;
+      } else {
+        telling.showStory();
+      }
+    }
+
+    saveAnswer() {
+      if (this.index > 0) {
+        answers.push(wordInput.value || '');
+      }
+      wordInput.value = '';
+      this.askForWord();
+    }
   }
-}
 
-// function to generate random number
-function random(min, max) {
-  return Math.floor(Math.random()*(max-min)) + min;
-}
+  // function to generate random number
+  function random(min, max) {
+    return Math.floor(Math.random()*(max-min)) + min;
+  }
 
-// initialize stories
-stories.push(new Story(
-  'Pretty Princess',
-  ['A new and ', ' fairy princess movie is coming out soon! It will be about ', ' and the ', ' dwarfs. ', ' is a princess whose beauty threatens her ', ' , the queen. ', ' is forced to flee from ', ' and hides in a nearby ', '. There, she discovers the dwarfs ', ' in their ', '. But the queen finds her and casts a ', ' spell on her. The dwarfs take care of her until the ', ' ', ' comes to rescue her, and they all live ', ' ever after!'],
-  new Words(['adjective', 'name/title', 'number', 'same name/title', 'relative', ' same name/title', 'place', 'place', 'verb/-ing', 'plural noun', 'adjective', 'adjective', 'person', 'adjective/-ly'])
-));
+  // initialize stories
+  stories.push(new Story(
+    'Pretty Princess',
+    ['A new and ', ' fairy princess movie is coming out soon! It will be about ', ' and the ', ' dwarfs. ', ' is a princess whose beauty threatens her ', ' , the queen. ', ' is forced to flee from ', ' and hides in a nearby ', '. There, she discovers the dwarfs ', ' in their ', '. But the queen finds her and casts a ', ' spell on her. The dwarfs take care of her until the ', ' ', ' comes to rescue her, and they all live ', ' ever after!'],
+    new Words(['adjective', 'name/title', 'number', 'same name/title', 'relative', ' same name/title', 'place', 'place', 'verb/-ing', 'plural noun', 'adjective', 'adjective', 'person', 'adjective/-ly'])
+  ));
 
-// randomly choose a story to display
-telling = stories[random(0, stories.length)];
+  // randomly choose a story to display
+  telling = stories[random(0, stories.length)];
 
-// display each input in sequence
-telling.words.askForWord();
-button.addEventListener('click', function() {
-  telling.words.saveAnswer();
-});
+  // display each input in sequence
+  telling.words.askForWord();
+  button.addEventListener('click', function() {
+    telling.words.saveAnswer();
+  });
+  
+})();

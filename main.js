821a523724ddde7d9cba860f3cdc673e681e562a
m-hatch <hatch@hq.acm.org>
2018-03-28 12:02:06 -0400

    add stories

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 (function(){
+
   // DOM elements
+  const sillyTitle = document.querySelector('.title');
   const wordInput = document.querySelector('.word__input');
   const wordtype = document.querySelector('.word__wordtype');
   const button = document.querySelector('.continue-btn');
@@ -24,6 +26,10 @@
       Story.id = Story.id || 0;
       Story.id++;
       return Story.id;
+    }
+
+    showTitle() {
+      sillyTitle.innerHTML = this.title;
     }
 
     showStory() {
@@ -80,11 +86,24 @@
     new Words(['adjective', 'name/title', 'number', 'same name/title', 'relative', ' same name/title', 'place', 'place', 'verb/-ing', 'plural noun', 'adjective', 'adjective', 'person', 'adjective/-ly'])
   ));
 
+  stories.push(new Story(
+    'Pizza Pizza',
+    ['Pizza was invented by a ', ' ', ' chef named ', '. To make pizza, you need to take a lump of ', ', and make a thin, round ', ' ', '. Then you cover it with ', ' sauce, ', ' cheese, and fresh chopped ', '. Next you have to bake it in a very hot ', '. When it is done, cut it into ', ' ', '. Some kids like ', ' pizza the best, but my favorite is the ', ' pizza. If I could, I would eat it ', ' times a day!'],
+    new Words(['adjective', 'nationality', 'person', 'noun', 'adjective', 'noun', 'adjective', 'adjective','plural noun', 'noun', 'number', 'shapes', 'food', 'food', 'number'])
+  ));
+
+  stories.push(new Story(
+    'What Happens in the Lunchroom, Stays in the Lunchroom',
+    ['Lunchtime in our cafeteria is always ', '. They serve hot ', 'and ', ', but some students ', ' their own ', ' to eat. Some kids quietly ', ' their ', ', while others throw ', ' or ', ' when the teachers aren&apos;t looking. One time, a bunch of kids mixed all of their unfinished ', ' and ', ' together to make a ', ' mountain of ', ' on a ', '. The teachers were ', ', but everyone had already ', ' outside for recess, so nobody got in trouble.'],
+    new Words(['adjective', 'food', 'food', 'verb', 'nouns', 'verb', 'food', 'vegetables', 'fruits', 'food', 'food', 'adjective', 'noun', 'furniture', 'emotion', 'verb/-ed'])
+  ));
+
   // randomly choose a story to display
   telling = stories[random(0, stories.length)];
-
-  // display each input in sequence
+  telling.showTitle();
   telling.words.askForWord();
+
+  // display each input in sequence and return silly story
   button.addEventListener('click', function() {
     telling.words.saveAnswer();
   });

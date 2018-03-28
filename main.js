6bbf4e2ff7868af589ed0c9d3c7fc0da4667b3ac
m-hatch <montehatch@gmail.com>
2018-03-27 22:15:39 -0400

    add story

--- main.js
+++ main.js
@@ -29,3 +29,18 @@ class Words {
     this.types = types;
   }
 }
+
+// function to generate random number
+function random(min, max) {
+  return Math.floor(Math.random()*(max-min)) + min;
+}
+
+// initialize stories
+stories.push(new Story(
+  'Pretty Princess',
+  ['A new and ', ' fairy princess movie is coming out soon! It will be about ', ' and the ', ' dwarfs. ', ' is a princess whose beauty threatens her ', ' , the queen. ', ' is forced to flee from ', ' and hides in a nearby ', '. There, she discovers the dwarfs ', ' in their ', '. But the queen finds her and casts a ', ' spell on her. The dwarfs take care of her until the ', ' ', ' comes to rescue her, and they all live ', ' ever after!'],
+  new Words(['adjective', 'name/title', 'number', 'same name/title', 'relative', ' same name/title', 'place', 'place', 'verb/-ing', 'plural noun', 'adjective', 'adjective', 'person', 'adjective/-ly'])
+));
+
+// randomly choose a story to display
+telling = stories[random(0, stories.length)];
